dad764a9bb26ae2053cad49c873446c3f8fae395
Abdullah90 <darkside944@gmail.com>
2022-09-24 11:23:59 +0300

    Add structure files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import ledger from "../src/assets/ledger.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ledger} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
