78f08ee17dae50f8138203efe9f195d46c4870f6
Hassan Mohammed <hassan-n98@hotmail.com>
2022-09-24 11:43:12 +0300

    Add home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import ledger from "../src/assets/ledger.png";
 import "./App.css";
-
+import Home from "../src/Pages/Home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ledger} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>company</div>
+      <div>budget</div>
+      <div>Yearly Net Profit</div>
+      <div>Survival Period</div>
+      <div>Comapny analytics</div>
+    </div>
+  );
+};
+
+export default Home;
