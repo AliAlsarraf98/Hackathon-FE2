003c8e6abc39f45b212f6a303fedaa95a67b832a
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-09-24 14:40:12 +0300

    feat: add company button component

---
+++ src/Components/AddCompany/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./style.module.scss";
+
+const AddCompany = () => {
+  return <div className={classes["company-button"]}>Add Company</div>;
+};
+
+export default AddCompany;
