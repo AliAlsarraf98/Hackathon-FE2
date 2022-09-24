32208c0d8dbbee143744a77b6155c0f44de54ab1
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-09-24 13:37:07 +0300

    feat: add budget button component

---
+++ src/Components/AddBudget/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./style.module.scss";
+
+const AddBudget = () => {
+  return <div className={classes["budget-button"]}>Add Initial Budget</div>;
+};
+
+export default AddBudget;
